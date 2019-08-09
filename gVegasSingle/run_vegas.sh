5d02ca4dffb510edab9326ce6da8424f1713eac3
Jalen Cates <jmcates@crimson.ua.edu>
2019-08-09 14:09:52 -0500

    Change script to submit jobs

--- gVegasSingle/run_vegas.sh
+++ gVegasSingle/run_vegas.sh
@@ -2,21 +2,9 @@
 
 ncall=1000
 itmx=10
+nblk=256
 
-if [ $# -gt 0 ];then
-    ncall=$1
-else
-    echo $0 ncall itmx nblk
-    exit
-fi
-if [ $# -gt 1 ];then
-    itmx=$2
-fi
-if [ $# -gt 2 ];then
-    nblk=$3
-fi
-
-echo run: gVegasSingle ${ncall} %{itmx} ${nblk}
+echo run: gVegasSingle ${ncall} ${itmx} ${nblk}
 
 gVegasSingle -n=${ncall} -i=${itmx} -b=${nblk}
 
