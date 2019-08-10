1746be1233bff18131e596323e79c65bbb3b8966
Jalen Cates <jmcates@crimson.ua.edu>
2019-08-10 13:44:53 -0500

    Remove for loop

--- gVegasSingle/gVegasFunc.cu
+++ gVegasSingle/gVegasFunc.cu
@@ -7,14 +7,17 @@ float func(float* rx, float wgt)
    float value = 0.f;
    float cosval = 0.f;
 
+   /*
    for (int i=0;i<7;i++) {
       value += rx[i];
    }
-   
-   cosval = __cosf(value);
+   */
+
+   value = rx[0] + rx[1] + rx[2] + rx[3] + rx[4] + rx[5] + rx[6]
+   cosval = cosf(value);
 
    return cosval;
-   
+
 }
 
 /*
