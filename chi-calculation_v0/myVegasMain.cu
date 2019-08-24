fd9ea74a3b04e80d6bb0011857b8225e1a4cb547
Jalen Cates <jmcates@crimson.ua.edu>
2019-08-24 17:00:38 -0500

    Changing number of calls

--- chi-calculation_v0/myVegasMain.cu
+++ chi-calculation_v0/myVegasMain.cu
@@ -34,7 +34,7 @@ int main(int argc, char* argv[])
    //   nBlockSize = nBlockSize0 is the size of the CUDA block
    //   ndim = ndim0 is the dimension of the integration space
 
-   int ncall0 = 1e4;
+   int ncall0 = 1e3;
    int itmx0 = 10;
    int nacc  = 1;
    int nBlockSize0 = 256;
