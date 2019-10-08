a13bf05656dd7efe4ed2311bca18a7c58ed47715
Jalen Cates - University of Alabama <ualjmc@dmcvlogin4.asc.edu>
2019-10-08 08:04:55 -0500

    Susceptibility folder

--- susceptibility/Regularized_v0/getrusage_sec.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-
-void getrusage_sec(double& utime, double& stime);
-double getrusage_usec();
-double getrusage_ssec();
--- susceptibility/Regularized_v0/kernels.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include "xorshift.cu"
-
-#include "gVegasFunc.cu"
-#include "gVegasCallFunc.cu"
-#include "gVegas.cu"
